6a44b0658128872188428e287a1c5817c6c23d49
Nora Rajeh <norarajeh20@gmail.com>
2023-10-03 09:53:33 -0400

    add the changes to index.html anf main.js files

--- js/main.js
+++ js/main.js
@@ -33,13 +33,16 @@ function init(){
         '','','',
         '','',''
     ];
+    render();
 }
 function render(){
-    board.foreach(function(mark,index){
+    board.forEach(function(mark,index){
        // console.log(mark, index);
        squares[index].textContent=mark;
     });
-    messages.textContent = `It's ${turn}'s turn!`;
+   // messages.textContent = `It's ${turn}'s turn!`;
+    messages.textContent = win === 'T' ? `That's a tie, queen!` : win ?
+     `${win} wins the game!` : `It's ${turn}'s turn!`;
     };
 function handleTurn(event){
     let idx= squares.findIndex(function(square){
@@ -47,6 +50,18 @@ function handleTurn(event){
     });
     board[idx]= turn;
     turn = turn ==='X' ? 'O' :'X';
+    win = getWinner();
     render();
     //console.log(board);
 }
+function getWinner(){
+    let winner = null;
+    winningCombos.forEach(function(combo, index) {
+        if (board[combo[0]] && board[combo[0]] === 
+            board[combo[1]] && board[combo[0]] === 
+            board[combo[2]]) winner = board[combo[0]];
+        });
+        return winner ? winner : board.includes('') ? null : 'T';
+
+}
+init();
